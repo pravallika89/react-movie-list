16701cf1302154c41ebc6fa602d5db0469e0ada0
meru20 <merhawitgubsa@gmail.com>
2021-03-16 18:50:03 -0700

    feat(movieList):added delete functionality for the movie list

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,6 +1,9 @@
 //import Table from 'react-bootstrap/Table';
+import { useState } from 'react';
+//import MovieDelete from "./MovieDelete";
 
 const MovieListComponent = ({movies}) => {
+    const [movieList, setMoviesList] =  useState(movies);
     const movieDetails =(movieId) => {
         let foundMovie = movies.find(movie => movie.id === movieId);
         if(!foundMovie){
@@ -8,6 +11,17 @@ const MovieListComponent = ({movies}) => {
         }
         else {
             alert (`${foundMovie.title}| ${foundMovie.genre} | ${foundMovie.year}`)
+        }
+    }
+    const movieDelete = (movieId) => {
+        let removeMovie = movieList.find((movie) => movie.id === movieId);
+        if( window.confirm(`Are you sure you want to remove ${removeMovie.title}?`)){
+            setMoviesList(movieList.filter(movie => movie.id !== movieId));
+            // console.log('movielist--->',movieList);
+            // console.log('movies---->',movies);
+        }
+        else {
+            alert("Sorry we don't have that Movie :(")
         }
     }
     return (
@@ -21,12 +35,14 @@ const MovieListComponent = ({movies}) => {
              <table className='table table-striped table-dark'>
                  <tbody>
                  {
-                   movies.map((movie) => {
+                   movieList.map((movie) => {
                   return (
                             <tr key={movie.id} >
                                 <td><h2 className='align-self-center'><a href={movie.imdbLink} target='_blank' rel='noreferrer'>{movie.title}</a></h2></td>
                                 <td><img  src={movie.image} alt='img' height='100px' width='100px'/></td>
                                 <td><button className='btn btn-primary justify-content-center' onClick={ () => movieDetails(movie.id)}>Details</button></td>
+                                <td><button className='btn btn-danger justify-content-center' onClick={ () => movieDelete(movie.id)}>Delete</button></td>
+                     
                             </tr>
 
                             
